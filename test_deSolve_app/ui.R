fe9b71d8de0f0b0f9b775277bdfa7d25d2f1b6b2
Jessica Wojciechowski <jessica.wojciechowski@mymail.unisa.edu.au>
2016-12-12 16:54:20 +1030

    ui update
    Dosing widgets now appear underneath the reactive plot object
    horizontally (was getting too long when they were all in the sidebar)
    Conditional panels weren’t working because of the “submit” button -
    button has been deleted

--- test_deSolve_app/ui.R
+++ test_deSolve_app/ui.R
@@ -2,11 +2,8 @@
 fixedPage(
 # Application Title and Logo
 	fixedRow(
-		column(10,
-			h2("PAGANZ - Shiny Example App"),
-			h6(""),
-    	offset = 1, align = "center"
-		)  #column
+		h2("PAGANZ - Shiny Example App"),
+		align = "center"
 	),	#fixedRow
 
 # Add a break with a horizontal line
@@ -15,110 +12,117 @@ fixedPage(
 # Sidebar panel with widgets
 	sidebarLayout(
 		sidebarPanel(
-		# Oral Dosing
-			checkboxInput("pocheck", "Oral Dosing", FALSE),
-			conditionalPanel("input.pocheck == true",
-			# Slider input for oral dose to be given every 24 hours
-				sliderInput("podose",
-					"Oral Dose (mg):",
-					min = 0, max = 1000, value = 500, step = 1
-				),  #sliderInput
-			# Slider input for oral dose frequency
-				sliderInput("potimes",
-					"Frequency of Dosing (times per day):",
-					min = 1, max = 4, value = 1, step = 1
-				)  #sliderInput
-			),  #conditionalPanel
-
-		# IV Bolus Dosing
-			checkboxInput("ivcheck", "IV Bolus Dosing"),
-			conditionalPanel("input.ivcheck == true",
-			# Slider input for IV bolus dose
-				sliderInput("ivdose",
-					"IV Bolus Dose (mg):",
-					min = 0, max = 1000, value = 500, step = 1
-				),  #sliderInput
-			# Slider input for IV bolus dose time
-				numericInput("ivtimes",
-					"IV Bolus Dose Time (hours):",
-					min = 0, max = 120, value = 0
-				)  #numericInput
-			),  #conditionalPanel
-
-		# Infusion Dosing
-			checkboxInput("infcheck", "Infusion Dosing"),
-			conditionalPanel("input.infcheck == true",
-			# Slider input for IV infusion dose
-				sliderInput("infdose",
-					"Infusion Dose (mg):",
-					min = 0, max = 4000, value = 2000, step = 1
-				),  #sliderInput
-			# Slider input for IV infusion duration
-				sliderInput("infdur",
-					"Infusion Duration (hours):",
-					min = 0, max = 24, value = 10, step = 1
-				),  #sliderInput
-			# Slider input for IV infusion starting time
-				sliderInput("inftimes",
-					"Infusion Start Time (hours):",
-					min = 0, max = 120, value = 72, step = 1
-				)  #sliderInput
-			),  #conditionalPanel
-
 		# Covariates
-			checkboxInput("covcheck", "Covariate Information"),
-			conditionalPanel("input.covcheck == true",
-			# Slider input for age
-				numericInput("age",
-					"Age (years):",
-					min = 0, max = 100, value = 32
-				),  #numericInput
-			# Slider input for weight
-				numericInput("wt",
-					"Total body weight (kg):",
-					min = 0, max = 72, value = 12
-				),  #numericInput
-			# Slider input for serum creatinine
-				numericInput("secr",
-					"Serum creatinine (umol/L):",
-					min = 0, max = 72, value = 12, step = 6
-				),  #numericInput
-			# Radio buttons for gender
-				selectInput("sex",
-					"Gender:",
-					choices = list(
-						"Male" = 0,
-						"Female" = 1),
-					selected = 1
-				),  #radioButtons
-			# Radio buttons for smoking status
-				radioButtons("smok",
-					"Smoking Status:",
-					choices = list(
-						"Not a Current Smoker" = 0,
-						"Current Smoker" = 1),
-					selected = 1
-				)  #radioButtons
-			),  #conditionalPanel
-
+		# Slider input for age
+			numericInput("age",
+				"Age (years):",
+				min = 0, max = 100, value = 32
+			),  #numericInput
+		# Slider input for weight
+			numericInput("wt",
+				"Total body weight (kg):",
+				min = 0, max = 72, value = 12
+			),  #numericInput
+		# Slider input for serum creatinine
+			numericInput("secr",
+				"Serum creatinine (µmol/L):",
+				min = 0, max = 72, value = 12, step = 6
+			),  #numericInput
+		# Radio buttons for gender
+			selectInput("sex",
+				"Gender:",
+				choices = list(
+					"Male" = 0,
+					"Female" = 1),
+				selected = 1
+			),  #radioButtons
+		# Radio buttons for smoking status
+			radioButtons("smok",
+				"Smoking Status:",
+				choices = list(
+					"Not a Current Smoker" = 0,
+					"Current Smoker" = 1),
+				selected = 1
+			),  #radioButtons
 		# Slider input for number of individuals
 			sliderInput("n",
 				"Number of Individuals:",
 				min = 10, max = 1000, value = 10, step = 10
-			),  #sliderInput
-
-			br(),
-
-		# Button to initiate simulation
-			submitButton("Simulate"),
-			align = "left"	 #sidebarPanel options
+			)  #sliderInput
 		),  #sidebarPanel
 
 	# Main panel to contain the concentration versus time plot
 		mainPanel(
 		# Plot output for concentration-time profile
-			plotOutput("plotconc", height = 650, width = 750),
-			align = "center"  #mainPanel options
+			fixedRow(
+				plotOutput("plotconc")
+			),	#fixedRow
+			hr(),
+			fixedRow(
+				column(4,
+				# Oral Dosing
+					checkboxInput("pocheck", "Oral Dosing", value = FALSE),
+					conditionalPanel(condition = "input.pocheck",
+					# Slider input for oral dose to be given every 24 hours
+						sliderInput("podose",
+							"Dose (mg):",
+							min = 0, max = 1000, value = 500, step = 100
+						),  #sliderInput
+					# Slider input for oral dose frequency
+						selectInput("potimes",
+							"Dosing Frequency:",
+							choices = list(
+								"Once daily (every 24 hours)" = 1,
+								"Twice daily (every 12 hours)" = 2,
+								"Three times a day (every 8 hours)" = 3,
+								"Four times a day (every 6 hours)" = 4
+							),
+							selected = 1
+						)  #selectInput
+					)	#conditionalPanel
+				),	#column
+				column(4,
+				# IV Bolus Dosing
+					checkboxInput("ivcheck", "IV Bolus Dosing", FALSE),
+					conditionalPanel(condition = "input.ivcheck == true",
+					# Slider input for IV bolus dose
+						sliderInput("ivdose",
+							"Dose (mg):",
+							min = 0, max = 1000, value = 500, step = 100
+						),  #sliderInput
+					# Slider input for IV bolus dose time
+						numericInput("ivtimes",
+							"Dose Time (hours):",
+							min = 0, max = 120, value = 0
+						)  #numericInput
+					)	#conditionalPanel
+				),	#column
+				column(4,
+				# IV Infusion Dosing
+					checkboxInput("infcheck", "IV Infusion Dosing", FALSE),
+					conditionalPanel(condition = "input.infcheck == true",
+					# Slider input for IV infusion dose
+						sliderInput("infdose",
+							"Dose (mg):",
+							min = 0, max = 4000, value = 2000, step = 400
+						),  #sliderInput
+					# Slider input for IV infusion duration
+						radioButtons("infdur",
+							"Duration (hours):",
+							choices = list(
+								"30 minutes" = 1,
+								"2 hours" = 2),
+							selected = 1,
+							inline = TRUE
+						),  #sliderInput
+					# Slider input for IV infusion starting time
+						numericInput("inftimes",
+							"Start Time (hours):",
+							min = 0, max = 120, value = 72, step = 1
+						)  #sliderInput
+					)	#conditionalPanel
+				)	#column
+			)	#fixedRow
 		)	 #mainPanel
 	)	#sidebarLayout
 )	#fixedPage
